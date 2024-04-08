724cae35c663b8838f2456656ae3f3300c326a2e
Samruddhi Khairnar <90044455+KBTUG20170@users.noreply.github.com>
2024-04-08 07:11:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.options('*',cors());
 var allowCrossDomain = function(req,res,next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header("Access-Control-Allow-Credentials" : true);
+  res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();  
